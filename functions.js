82f11223949ab5b278d6b80400b450a107111ca0
viooltje <chantal.houtschild@outlook.com>
2021-05-02 20:20:03 +0200

    Add ReadMe + JS file with functions - function that add seven to a number - function that multiply the parameters - function that gives the last letter of a string - function that that only returns the string with only at the beginning a capital letter

---
+++ functions.js
@@ -0,0 +1,15 @@
+function add7(number) {
+    return number + 7;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function capitalize(string){
+    return;
+}
+
+function lastLetter(string) {
+    return string[-1];
+}
