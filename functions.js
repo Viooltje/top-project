0c2a88f5785035b9be3f6ce5d130be04a8715b7c
viooltje <chantal.houtschild@outlook.com>
2021-05-03 20:10:19 +0200

    Tweaked code + workedon capitalize. - return --> console.log - Function capitalize does not work yet, don't know what is wrong with it yet

--- functions.js
+++ functions.js
@@ -1,15 +1,30 @@
+add7(7);
+multiply(5, 2);
+lastLetter("abcd");
+capitalize("HEEEY how aRe you");
+
+
 function add7(number) {
-    return number + 7;
+    console.log(number + 7);
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+    console.log(number1 * number2) ;
 }
 
-function capitalize(string){
-    return;
+function capitalize(string) {
+    let newstr = "";
+    for (var i = 0; i < string.length; i++) {
+        if (i = 0) {
+            newstr += string[i].toUpperCase;
+        } else {
+            newstr += string[i].toLowerCase;
+        }
+        console.log(newstr);
+    }
+    
 }
 
 function lastLetter(string) {
-    return string[-1];
+    console.log(string.slice(-1));
 }
